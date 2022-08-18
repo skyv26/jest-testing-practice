5868c7353056517449665a7508113c75b079547b
skyv26 <dev.aakashv@gmail.com>
2022-08-18 19:05:24 +0530

    add `task-3` for testing
    - created `Calculator` class.
    - created `add`, `subtract`, `multiply` and `divide` functions.
    - added `test` for `Calculator` atleast 3 *cases*.

--- taskFunction.test.js
+++ taskFunction.test.js
@@ -1,4 +1,6 @@
-const { stringLength, reverseString } = require('./taskFunction');
+const { stringLength, reverseString, Calculator } = require('./taskFunction');
+
+const calc = new Calculator();
 
 // Check the length of string
 test('String `Aakash Verma` should return 12 as length', () => {
@@ -18,4 +20,37 @@ test('String `Lorem Ipsum` should throw an Error', () => {
 // Check whether we are getting the right reverse version of original string or not
 test('String `Aakash` should be equal to `hsakaA`', () => {
   expect(reverseString('Aakash')).toMatch('hsakaA');
+});
+
+// Calculator test
+describe('Calculator', () => {
+  it('::add 5 + 4 should be equal to 9', () => {
+    expect(calc.add(5,4)).toBe(9)
+  })
+
+  it('::add 5 + 4 should not be equal to 9', () => {
+    expect(calc.add(5,4)).not.toBe(12)
+  })
+
+  
+  it('::subtract 4 - 5 should be equal to -1', () => {
+    expect(calc.subtract(4,5)).toBe(-1)
+  })
+
+  it('::subtract 4 - 5 should not be equal to -1', () => {
+    expect(calc.subtract(4,5)).not.toBe(1)
+  })
+
+  it('::multiply 4 * 5 should be equal to 20', () => {
+    expect(calc.multiply(4,5)).toBe(20)
+  })
+
+  it('::divide 4 / 5 should be equal to 0.8', () => {
+    expect(Number(calc.divide(4,5).toFixed(1))).toBe(0.8)
+  })
+
+  it('::divide 4 / 5 should not be equal to 0.8', () => {
+    expect(calc.divide(4,5).toFixed(1)).not.toBe(0.8)
+  })
+  
 });
